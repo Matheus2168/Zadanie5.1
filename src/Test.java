fe816c254a07f490dcc622c68992089a5065e6ba
Mateusz <mateuszhotowy@gmail.com>
2017-12-08 09:09:18 +0100

    Done.

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Ania","Ola","Ada"};
+
+        for (int i = 0 ; i < names.length ; i++)
+            System.out.println(names[i]);
+    }
+}
